b0492dac0f01953d1de506d7e94071ace643f04f
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2023-08-11 14:26:22 +0200

    Initial crypto commitment.

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    `maven-publish`
+}
+
+group = "org.angproj.crypt"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+kotlin {
+    withSourcesJar(publish = true)
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(IR) {
+        binaries.executable()
+        browser {
+        }
+        generateTypeScriptDefinitions()
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.angproj.aux.util:angelos-project-aux:0.0.1")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+
+        }
+    }
+    publications {
+        getByName<MavenPublication>("kotlinMultiplatform") {
+            artifactId = rootProject.name
+        }
+    }
+}
