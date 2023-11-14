818389db698f109f6d80146873d93e08cfd335ad
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2023-11-14 22:10:35 +0100

    Going forth!

--- src/commonMain/kotlin/org/angproj/crypt/ripemd/Ripemd160Hash.kt
+++ src/commonMain/kotlin/org/angproj/crypt/ripemd/Ripemd160Hash.kt
@@ -14,36 +14,15 @@
  */
 package org.angproj.crypt.ripemd
 
-import org.angproj.aux.util.EndianAware
-import org.angproj.aux.util.readIntAt
-import org.angproj.aux.util.writeIntAt
 import org.angproj.crypt.Hash
-import org.angproj.crypt.HashEngine
 
-/**
- * https://homes.esat.kuleuven.be/~bosselae/ripemd160.html
- *
- * https://homes.esat.kuleuven.be/~bosselae/ripemd/rmd128.txt
- * https://homes.esat.kuleuven.be/~bosselae/ripemd/rmd160.txt
- * https://homes.esat.kuleuven.be/~bosselae/ripemd/rmd256.txt
- * https://homes.esat.kuleuven.be/~bosselae/ripemd/rmd320.txt
- */
+internal class Ripemd160Hash: AbstractRipemdHashEngine() {
 
-internal class Ripemd160Hash: HashEngine, EndianAware {
-
-    val h: IntArray = intArrayOf(
+    override val h: IntArray = intArrayOf(
         0x67452301, -0x10325477, -0x67452302, 0x10325476, -0x3c2d1e10
     )
 
-    protected var lasting: ByteArray = ByteArray(0)
-
-    protected var count: Int = 0
-
-    private fun push(chunk: ByteArray): IntArray = IntArray(16).also {
-        it.indices.forEach { idx -> it[idx] = chunk.readIntAt(idx * wordSize).asLittle() }
-    }
-
-    private fun transform(x: IntArray) {
+    override fun transform(x: IntArray) {
         val h0 = h.copyOf()
         val h1 = h.copyOf()
 
@@ -61,49 +40,7 @@ internal class Ripemd160Hash: HashEngine, EndianAware {
         h[0] = h1[3]
     }
 
-    override fun update(messagePart: ByteArray) {
-        val buffer = lasting + messagePart
-        lasting = ByteArray(0) // Setting an empty array
-
-        (0..buffer.size step blockSize).forEach { i ->
-
-            // Slicing the buffer in ranges of 64, if too small it's lasting.
-            val chunk = try {
-                messagePart.copyOfRange(i, i + blockSize)
-            } catch (_: IndexOutOfBoundsException) {
-                lasting = buffer.copyOfRange(i, buffer.size)
-                return
-            }
-
-            transform(push(chunk))
-            count += blockSize
-        }
-    }
-
-    override fun final(): ByteArray {
-        count += lasting.size
-        val bitCount: Long = count * 8L // WARNING! It's sensitive that bitCount is a Long.
-        val buffer = lasting + byteArrayOf(128.toByte(), 0, 0, 0).copyOfRange(0, 4 - lasting.size.rem(4))
-
-        val x = IntArray(16)
-        val wordCount = buffer.size.floorDiv(wordSize)
-        (0 until wordCount).forEach { idx -> x[idx] = buffer.readIntAt(idx * wordSize).asLittle() }
-
-        if(wordCount > 14) {
-            transform(x)
-            x.fill(0)
-        }
-
-        x[14] = (bitCount and -0x1).toInt()
-        x[15] = (bitCount ushr Int.SIZE_BITS).toInt()
-        transform(x)
-
-        val hash = ByteArray(h.size * wordSize)
-        h.indices.forEach { hash.writeIntAt(it * wordSize, h[it].asLittle()) }
-        return truncate(hash)
-    }
-
-    fun truncate(hash: ByteArray): ByteArray = hash
+    //fun truncate(hash: ByteArray): ByteArray = hash
 
     override val type: String
         get() = "RIPEMD"
@@ -117,7 +54,7 @@ internal class Ripemd160Hash: HashEngine, EndianAware {
         public override fun create(): Ripemd160Hash = Ripemd160Hash()
 
         private fun round(x: IntArray, h0: IntArray, h1: IntArray, range: IntRange, k0: Int, k1: Int) {
-            var t = 0
+            var t: Int
             range.forEach { j ->
                 t = (h0[0] + f(j, h0[1], h0[2], h0[3]) + x[r0[j]] + k0).rotateLeft(s0[j]) + h0[4]
                 h0[0] = h0[4]
@@ -135,48 +72,7 @@ internal class Ripemd160Hash: HashEngine, EndianAware {
             }
         }
 
-        private fun f(j: Int, x: Int, y: Int, z: Int): Int = when(j) {
-            in 0..15 -> x xor y xor z
-            in 16..31 -> (x and y) or (x.inv() and z)
-            in 32..47 -> (x or y.inv()) xor z
-            in 48..63 -> (x and z) or (y and z.inv())
-            in 64..79 -> x xor (y or z.inv())
-            else -> error("Can't happen: $j")
-        }
-
         private val k0 = intArrayOf(0x00000000, 0x5A827999, 0x6ED9EBA1, -0x70e44324, -0x56ac02b2)
         private val k1 = intArrayOf(0x50A28BE6, 0x5C4DD124, 0x6D703EF3, 0x7A6D76E9, 0x000000000)
-
-        private val r0 = intArrayOf(
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-            7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8,
-            3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12,
-            1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2,
-            4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13
-        )
-
-        private val r1 = intArrayOf(
-            5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12,
-            6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2,
-            15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13,
-            8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14,
-            12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11
-        )
-
-        private val s0 = intArrayOf(
-            11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
-            7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
-            11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
-            11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
-            9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6
-        )
-
-        private val s1 = intArrayOf(
-            8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6,
-            9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11,
-            9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5,
-            15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8,
-            8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11
-        )
     }
 }
---
+++ src/jvmVectorTest/kotlin/crypt/hmac/Ripemd160KeyHashedMacTest.kt
@@ -0,0 +1,57 @@
+package crypt.hmac
+
+import crypt.ripemd.RipemdMessages
+import org.angproj.aux.util.BinHex
+import org.angproj.crypt.hmac.KeyHashedMac
+import org.angproj.crypt.ripemd.Ripemd160Hash
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Ripemd160KeyHashedMacTest {
+
+    /**
+     * Test vectors and keys from
+     * https://homes.esat.kuleuven.be/~bosselae/ripemd160.html
+     */
+
+    val keys = listOf(
+        "00112233445566778899aabbccddeeff01234567",
+        "0123456789abcdeffedcba987654321000112233"
+    )
+
+    val testVectorsDigest = arrayListOf(
+        listOf(
+            "cf387677bfda8483e63b57e06c3b5ecd8b7fc055",
+            "0d351d71b78e36dbb7391c810a0d2b6240ddbafc",
+            "f7ef288cb1bbcc6160d76507e0a3bbf712fb67d6",
+            "f83662cc8d339c227e600fcd636c57d2571b1c34",
+            "843d1c4eb880ac8ac0c9c95696507957d0155ddb",
+            "60f5ef198a2dd5745545c1f0c47aa3fb5776f881",
+            "e49c136a9e5627e0681b808a3b97e6a6e661ae79",
+            "31be3cc98cee37b79b0619e3e1c2be4f1aa56e6c",
+            "c2aa88c6405658dc225e485488371fb2433fa735"
+        ),
+        listOf(
+            "fe69a66c7423eea9c8fa2eff8d9dafb4f17a62f5",
+            "85743e899bc82dbfa36faaa7a25b7cfd372432cd",
+            "6e4afd501fa6b4a1823ca3b10bd9aa0ba97ba182",
+            "2e066e624badb76a184c8f90fba053330e650e92",
+            "07e942aa4e3cd7c04dedc1d46e2e8cc4c741b3d9",
+            "b6582318ddcfb67a53a67d676b8ad869aded629a",
+            "f1be3ee877703140d34f97ea1ab3a07c141333e2",
+            "85f164703e61a63131be7e45958e0794123904f9",
+            "82a504a002ba6e6c67f3cd67cedb66dc169bab7a"
+        )
+    )
+
+    @Test
+    fun testRipemd160Msg() {
+        keys.forEachIndexed { num, key ->
+            RipemdMessages.testVectors.forEachIndexed { idx, msg ->
+                val hmac = KeyHashedMac.create(BinHex.decodeToBin(key), Ripemd160Hash)
+                hmac.update(msg)
+                assertEquals(BinHex.encodeToHex(hmac.final()), testVectorsDigest[num][idx].lowercase())
+            }
+        }
+    }
+}
