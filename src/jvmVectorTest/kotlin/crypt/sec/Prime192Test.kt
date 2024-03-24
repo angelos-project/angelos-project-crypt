d2c1ac65ef7ab7a850901797fc81e42eb2bdc8c8
kristoffer-paulsson <kristoffer.paulsson@talenten.se>
2024-03-24 17:23:23 +0100

    Added checks for properly entered curves but in decimal comparison.

--- src/jvmVectorTest/kotlin/crypt/sec/Prime192Test.kt
+++ src/jvmVectorTest/kotlin/crypt/sec/Prime192Test.kt
@@ -1,8 +1,50 @@
 package crypt.sec
 
+import org.angproj.crypt.sec.Secp192Random1
+import java.math.BigInteger
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Prime192Test {
+
+    /**
+     * From NIST SP.800-186, 3.2.1.2, P-192, p.10
+     * Information missing, refers to FIPS 186-4.
+     *
+     * D.1.2.1 Curve P-192
+     * "The integers p and n are given in decimal form; bit strings and field elements are given in
+     * hexadecimal."
+     *
+     * field a is not given!
+     * */
+    @Test
+    fun testOutputOfBigInteger () {
+        val dp = Secp192Random1.domainParameters
+        /*assertEquals(
+            Secp192r1.a.toString(),
+            ""
+        )*/
+        assertEquals(
+            BigInteger(dp.b.value.toByteArray()).toString(16),
+            "64210519" + "e59c80e7" + "0fa7e9ab" + "72243049" + "feb8deec" + "c146b9b1"
+        )
+        assertEquals(
+            BigInteger(dp.n.value.toByteArray()).toString(),
+            "6277101735386680763835789423176059013767194773182842284081"
+        )
+        assertEquals(
+            BigInteger(dp.p.value.toByteArray()).toString(),
+            "6277101735386680763835789423207666416083908700390324961279"
+        )
+        assertEquals(
+            BigInteger(dp.G.x.value.toByteArray()).toString(16),
+            "188da80e" + "b03090f6" + "7cbf20eb" + "43a18800" + "f4ff0afd" + "82ff1012"
+        )
+        assertEquals(
+            BigInteger(dp.G.y.value.toByteArray()).toString(16),
+            "7192b95" + "ffc8da78" + "631011ed" + "6b24cdd5" + "73f977a1" + "1e794811"
+        )
+    }
 
     @Test
     fun testKeyPair () {
--- src/jvmVectorTest/kotlin/crypt/sec/Prime224Test.kt
+++ src/jvmVectorTest/kotlin/crypt/sec/Prime224Test.kt
@@ -1,6 +1,44 @@
 package crypt.sec
 
+import junit.framework.TestCase
+import org.angproj.crypt.sec.Secp224Random1
+import org.junit.Test
+import java.math.BigInteger
+
 class Prime224Test {
+
+    /**
+     * From NIST SP.800-186, 3.2.1.2, P-224, p.10
+     * */
+    @Test
+    fun testOutputOfBigInteger () {
+        val dp = Secp224Random1.domainParameters
+        TestCase.assertEquals(
+            BigInteger(dp.a.value.toByteArray()).toString(),
+            "26959946667150639794667015087019630673557916260026308143510066298878"
+        )
+        TestCase.assertEquals(
+            BigInteger(dp.b.value.toByteArray()).toString(),
+            "18958286285566608000408668544493926415504680968679321075787234672564"
+        )
+        TestCase.assertEquals(
+            BigInteger(dp.n.value.toByteArray()).toString(),
+            "26959946667150639794667015087019625940457807714424391721682722368061"
+        )
+        TestCase.assertEquals(
+            BigInteger(dp.p.value.toByteArray()).toString(),
+            "26959946667150639794667015087019630673557916260026308143510066298881"
+        )
+        TestCase.assertEquals(
+            BigInteger(dp.G.x.value.toByteArray()).toString(),
+            "19277929113566293071110308034699488026831934219452440156649784352033"
+        )
+        TestCase.assertEquals(
+            BigInteger(dp.G.y.value.toByteArray()).toString(),
+            "19926808758034470970197974370888749184205991990603949537637343198772"
+        )
+    }
+
     object P_224_KeyPair {
         val testVectors: String = """#  CAVS 11.1
 #  "Key Pair" information 
