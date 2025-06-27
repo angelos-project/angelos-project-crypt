110a356d12614ab151869d9504f93d698e1fa88f
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-06-27 20:04:13 +0200

    Fixed coverage

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
     alias(libs.plugins.dokka)
-    jacoco
+    alias(libs.plugins.kover)
 }
 
 group = "org.angproj.crypt"
@@ -148,38 +148,11 @@ tasks.dokkaHtml {
     }
 }
 
-jacoco {
-    toolVersion = "0.8.12"
-    reportsDirectory.set(layout.buildDirectory.dir("reports/jacoco"))
-}
-
-tasks {
-    withType<Test> {
-        finalizedBy(withType(JacocoReport::class))
-    }
-    register("jacocoTestReport", JacocoReport::class) {
-        dependsOn(withType(Test::class))
-        val coverageSourceDirs = arrayOf(
-            "src/commonMain",
-        )
-
-        val buildDirectory = layout.buildDirectory
-
-        val classFiles = buildDirectory.dir("classes/kotlin/jvm").get().asFile
-            .walkBottomUp()
-            .toSet()
-
-        classDirectories.setFrom(classFiles)
-        sourceDirectories.setFrom(files(coverageSourceDirs))
-
-        buildDirectory.files("jacoco/jvmTest.exec").let {
-            executionData.setFrom(it)
-        }
-
-        reports {
-            xml.required.set(true)
-            csv.required.set(true)
-            html.required.set(true)
+kover {
+    reports {
+        total {
+            xml.onCheck.set(true)
+            html.onCheck.set(true)
         }
     }
 }
